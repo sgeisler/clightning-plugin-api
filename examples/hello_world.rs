7efbd8fb9450e5a91935096d38c5f510a83b8c53
Sebastian Geisler <sebastian@blockstream.io>
2019-03-11 21:42:25 -0700

    add new hello world example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,46 +1,77 @@
 extern crate clightning_plugin_api;
 extern crate clightningrpc;
-#[macro_use] extern crate serde_derive;
+extern crate serde_derive;
 
 use clightning_plugin_api::{NoOptions, Plugin, PluginContext, RpcMethod, RpcMethodParams};
-use std::sync::atomic::{AtomicBool, Ordering};
 
-struct TestContext {
-    state: AtomicBool,
+#[derive(Debug, Eq, PartialEq)]
+struct HelloWorldRequest {
+    name: Option<String>,
+    message: Option<String>,
 }
 
-#[derive(Deserialize)]
-struct TestRequest {
-    state: bool,
+impl<'de> serde::Deserialize<'de> for HelloWorldRequest {
+    fn deserialize<D>(deserializer: D) -> Result<HelloWorldRequest, D::Error>
+        where D: serde::Deserializer<'de>
+    {
+        // Should handle wrong number of arguments error better
+        let params: Vec<String> = serde::Deserialize::deserialize(deserializer)?;
+        let mut param_iter = params.into_iter();
+        Ok(HelloWorldRequest {
+            name: param_iter.next(),
+            message: param_iter.next()
+        })
+    }
 }
 
 // This would be automaticcally derived
-impl RpcMethodParams for TestRequest {
+impl RpcMethodParams for HelloWorldRequest {
     fn usage() -> &'static str {
-        "new_state"
+        "[name] [message]"
     }
 }
 
 
 fn main() {
-    // {"jsonrpc":"2.0","method":"getmanifest","id":"aloha"}
-    let ctx = TestContext {
-        state: AtomicBool::new(false),
-    };
-
-    let mut plugin = Plugin::<NoOptions, _>::with_context(&ctx)
+    let mut plugin = Plugin::<NoOptions, ()>::new()
         .mount_rpc(RpcMethod::new(
             "hello_world",
             "test rpc call that changes some boolean state",
-            |ctx: PluginContext<NoOptions, TestContext>, request: TestRequest| {
-                let old = ctx.context.state.swap(request.state, Ordering::Relaxed);
-                Ok(if old != request.state {
-                    "State changed!"
-                } else {
-                    "State didn't change."
-                })
+            |_ctx: PluginContext<NoOptions, ()>, request: HelloWorldRequest| {
+                let mut response = String::new();
+
+                response += &format!(
+                    "Hello {}!",
+                    request.name.unwrap_or("World".into())
+                );
+
+                if let Some(message) = request.message {
+                    response += &format!(" Your message: {}", message);
+                }
+
+                Ok(response)
             }
         ));
 
     plugin.run();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::HelloWorldRequest;
+
+    #[test]
+    fn de_option() {
+        let json = "[\"test\", \"test2\"]";
+        let parsed: HelloWorldRequest = serde_json::from_str(json).unwrap();
+        assert_eq!(HelloWorldRequest {name: Some("test".into()), message: Some("test2".into())}, parsed);
+
+        let json = "[\"test\"]";
+        let parsed: HelloWorldRequest = serde_json::from_str(json).unwrap();
+        assert_eq!(HelloWorldRequest {name: Some("test".into()), message: None}, parsed);
+
+        let json = "[]";
+        let parsed: HelloWorldRequest = serde_json::from_str(json).unwrap();
+        assert_eq!(HelloWorldRequest {name: None, message: None}, parsed);
+    }
 }
