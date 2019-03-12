c1fea1976eb9bf170b36cc31ddede8dbd846a5b3
Sebastian Geisler <sebastian@blockstream.io>
2019-03-11 21:42:08 -0700

    move state example

---
+++ examples/state.rs
@@ -0,0 +1,46 @@
+extern crate clightning_plugin_api;
+extern crate clightningrpc;
+#[macro_use] extern crate serde_derive;
+
+use clightning_plugin_api::{NoOptions, Plugin, PluginContext, RpcMethod, RpcMethodParams};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::spawn;
+
+struct TestContext {
+    state: AtomicBool,
+}
+
+#[derive(Deserialize)]
+struct TestRequest {
+    state: bool,
+}
+
+// This would be automaticcally derived
+impl RpcMethodParams for TestRequest {
+    fn usage() -> &'static str {
+        "new_state"
+    }
+}
+
+
+fn main() {
+    let ctx = TestContext {
+        state: AtomicBool::new(false),
+    };
+
+    let mut plugin = Plugin::<NoOptions, _>::with_context(&ctx)
+        .mount_rpc(RpcMethod::new(
+            "set_state",
+            "test rpc call that changes some boolean state",
+            |ctx: PluginContext<NoOptions, TestContext>, request: TestRequest| {
+                let old = ctx.context.state.swap(request.state, Ordering::Relaxed);
+                Ok(if old != request.state {
+                    "State changed!"
+                } else {
+                    "State didn't change."
+                })
+            }
+        ));
+
+    plugin.run();
+}
